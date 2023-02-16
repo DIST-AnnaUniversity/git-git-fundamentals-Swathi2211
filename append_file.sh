420fb8514cd8f927567ca174ff9021009b8bb23f
Swathi2211 <121076940+Swathi2211@users.noreply.github.com>
2023-02-16 22:51:30 +0530

    Add files via upload

---
+++ append_file.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Before appending the file"
+cat abc.txt
+
+echo "Learning Laravel 5">> abc.txt
+echo "After appending the file"
+cat abc.txt
---
+++ comment_example.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Add two numeric value
+((sum=2+3))
+
+##print the sum
+echo $sum
